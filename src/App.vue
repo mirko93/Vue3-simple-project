8cc956e3525b7c139fa98c7f805e0acee1b8a07c
mirko93 <skopljak.mirko93@gmail.com>
2021-01-26 10:56:21 +0100

    create template, conditionals, loops & events

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <UserProfile />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserProfile from './components/UserProfile.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    UserProfile,
   }
 }
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+  margin: 5px;
+  color: black;
 }
 </style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="post-item" @click="favouritePost(post.id)">
+        <div class="user-prfole__post">
+            <div class="post-item__user">
+                @{{ username }}
+            </div>
+
+            <div class="post-item__content">
+                {{ post.content }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostItem",
+
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+
+        post: {
+            type: Object,
+            required: true
+        }
+    },
+
+    methods: {
+        favouritePost(id) {
+            this.$emit('favourite', id);
+        }
+    }
+}
+</script>
+
+<style>
+.post-item {
+    padding: 20px;
+    margin: 5px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.post-item:hover {
+    transform: scale(1.1, 1.1);
+}
+
+.post-item__user {
+    font-weight: bold;
+}
+
+</style>
